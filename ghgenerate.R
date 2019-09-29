79bd5f8035a59b712407ee73d64c5421d6aa6fe2
Mohammed Ali <mali@uplandsoftware.com>
2019-09-29 12:24:28 +0200

    add docs

--- ghgenerate.R
+++ ghgenerate.R
@@ -6,3 +6,5 @@ rmarkdown::render(proposal.file, output_format="html_document",
                   output_dir="out", quiet=TRUE)
 rmarkdown::render(proposal.file, output_format="pdf_document",
                   output_dir="out", quiet=TRUE)
+rmarkdown::render(proposal.file, output_format="github_document",
+                  output_dir="out", quiet=TRUE)
